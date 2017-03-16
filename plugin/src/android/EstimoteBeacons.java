b0cd443d1f4b54fd7fd58c72dfd49a0aef4c4231
Rafael Diaz <rd@lanista-training.com>
2017-03-16 15:24:03 +0100

    Test

--- plugin/src/android/EstimoteBeacons.java
+++ plugin/src/android/EstimoteBeacons.java
@@ -233,7 +233,7 @@ public class EstimoteBeacons extends CordovaPlugin
 	{
 		Log.i(LOGTAG, "startRangingBeaconsInRegion");
 		final UUID ESTIMOTE_PROXIMITY_UUID = UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D");
-    final Region ALL_ESTIMOTE_BEACONS = new Region("rid", ESTIMOTE_PROXIMITY_UUID, null, null);
+    final region ALL_ESTIMOTE_BEACONS = new Region("rid", ESTIMOTE_PROXIMITY_UUID, null, null);
 
 		String key = regionHashMapKey(region);
 		if (null != mRangingCallbackContexts.get(key)) {
